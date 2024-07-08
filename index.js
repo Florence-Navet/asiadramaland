cfd507af8150f24c38e468358cbfa2f5105db8e2
enolaworld <enolaworld@gmail.com>
2024-07-08 17:59:32 +0200

    arrangement navbar,fin section 2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const nav = document.querySelector("nav");
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
 
+window.addEventListener("scroll", () => {
   if (window.scrollY > 120) {
-    nav.style.top = 0;
+    nav.style.top = "0";
   } else {
     nav.style.top = "-50px";
   }
