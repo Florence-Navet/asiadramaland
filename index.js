d5ccd132f9649ebf121e2ff37e2c36ca1cbb4b69
enolaworld <enolaworld@gmail.com>
2024-07-11 09:39:05 +0200

    menu burger, finition media query

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ window.addEventListener("scroll", () => {
     nav.style.top = "-50px";
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.getElementById("burger-menu");
+  const navElements = document.getElementById("nav-elements");
+  const nav = document.querySelector("nav");
+
+  burgerMenu.addEventListener("click", function () {
+    navElements.classList.toggle("active");
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 120) {
+      nav.style.top = "0";
+    } else {
+      nav.style.top = "-50px";
+    }
+  });
+});
