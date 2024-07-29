03f0f97200b119796816dd9439af91489ede2165
enolaworld <enolaworld@gmail.com>
2024-07-29 12:59:26 +0200

    modif nav bar 80px

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", () => {
   if (window.scrollY < lastScroll) {
     navbar.style.top = 0;
   } else {
-    navbar.style.top = "-60px";
+    navbar.style.top = "80px";
   }
 
   lastScroll = window.scrollY;
